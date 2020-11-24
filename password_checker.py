7194e353fbca524f5914dfccff5538eb6a45f43b
drgreenthumb93 <50826299+drgreenthumb93@users.noreply.github.com>
2020-11-24 11:04:06 +0100

    Create password_checker.py

---
+++ password_checker.py
@@ -0,0 +1,32 @@
+
+# Komplexität von Passwörtern überprüfen
+
+import re 
+password = "" # <-- Passwort hier eintippen
+flag = 0
+while True:   
+    if (len(password)<8): 
+        flag = -1
+        break
+    elif not re.search("[a-z]", password): 
+        flag = -1
+        break
+    elif not re.search("[A-Z]", password): 
+        flag = -1
+        break
+    elif not re.search("[0-9]", password): 
+        flag = -1
+        break
+    elif not re.search("[_@$]", password): 
+        flag = -1
+        break
+    elif re.search("\s", password): 
+        flag = -1
+        break
+    else: 
+        flag = 0
+        print("Ausreichendes Passwort") 
+        break
+  
+if flag ==-1: 
+    print("Ungenügendes Passwort") 
